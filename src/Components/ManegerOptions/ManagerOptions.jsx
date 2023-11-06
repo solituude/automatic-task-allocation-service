57ac97a1fc9e58d6bf6d4f3c8aca08245ae0be6a
solituude <dbelousova70@gmail.com>
2023-11-06 14:52:10 +0400

    Added options list on manager homepage

--- src/Components/ManegerOptions/ManagerOptions.jsx
+++ src/Components/ManegerOptions/ManagerOptions.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import s from './managerOptions.module.scss'
+import rightIcon from '../../assets/homepage/rightIcon.svg';
+import infoIcon from '../../assets/homepage/infoIcon.svg';
+import updateIcon from '../../assets/homepage/updateIcon.svg';
+import distributeIcon from '../../assets/homepage/distributeIcon.svg';
+import {NavLink} from "react-router-dom";
 
 const ManagerOptions = () => {
     return(
-        <div></div>
+        <div className={s.content}>
+            <NavLink to='/pointsInfo' className={s.option__container} style={{textDecoration: "none"}}>
+                <img src={rightIcon} alt={"<"}/>
+                <span className={s.option__label}>Информация по точкам</span>
+                <img src={infoIcon} alt={"i"}/>
+            </NavLink>
+
+            <NavLink to='/updateHandBook' className={s.option__container} style={{textDecoration: "none"}}>
+                <img src={rightIcon} alt={"<"}/>
+                <span className={s.option__label}>Редактировать справочники</span>
+                <img src={updateIcon} alt={"i"}/>
+            </NavLink>
+
+            <NavLink to='/taskDistribution' className={s.option__container} style={{textDecoration: "none"}}>
+                <img src={rightIcon} alt={"<"}/>
+                <span className={s.option__label}>Распределить задачи</span>
+                <img src={distributeIcon} alt={"i"}/>
+            </NavLink>
+        </div>
     )
 }
 
--- src/Pages/ManagerHomepage/ManagerHomepage.jsx
+++ src/Pages/ManagerHomepage/ManagerHomepage.jsx
@@ -3,15 +3,17 @@ import Header from "../../UI/Header/Header";
 import EmployeeSlider from "../../UI/EmployeeSlider/EmployeeSlider";
 import s from './managerHomepage.module.scss'
 import {ReportButton} from "../../UI/ReportButton/ReportButton";
+import ManagerOptions from "../../Components/ManegerOptions/ManagerOptions";
 
 const ManagerHomepage = () => {
     return(
-        <div className={s.content}>
+        <div className={s.container}>
             <Header/>
             <EmployeeSlider/>
-
-            <ReportButton/>
-
+            <div className={s.content}>
+                <ReportButton/>
+                <ManagerOptions/>
+            </div>
         </div>
     )
 }
