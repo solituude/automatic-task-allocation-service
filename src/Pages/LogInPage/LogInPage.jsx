272ca616447f627b5c15d1711dcd7406dd9956ad
solituude <dbelousova70@gmail.com>
2023-11-06 08:11:30 +0400

    Added login page

--- src/Pages/LogInPage/LogInPage.jsx
+++ src/Pages/LogInPage/LogInPage.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import {Container} from "react-bootstrap";
 import {BackButton} from "../../UI/BackButton/BackButton";
+import s from '../SignUpPage/signUpPage.module.scss';
+import NameForm from "../../Components/NameForm/NameForm";
+import PasswordForm from "../../Components/PasswordForm/PasswordForm";
 
 const LogInPage = () => {
-    return(
-        <Container>
-            <div>
-                <BackButton/>
-            </div>
-        </Container>
+    return (
+        <div className={s.content}>
+            <BackButton/>
+            <span className={s.header__label}>Вход</span>
+            <form className={s.form__container}>
+                <NameForm/>
+                <PasswordForm/>
+                <button type={"submit"} className={s.submit__button}>
+                    <span className={s.submit__label}>
+                        Войти
+                    </span>
+                </button>
+            </form>
+        </div>
     )
 }
-
 export default LogInPage;
